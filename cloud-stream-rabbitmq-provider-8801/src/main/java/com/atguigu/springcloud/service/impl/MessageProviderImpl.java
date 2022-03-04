7a19861f581ddd875b58b44ae9eecff543097e15
axh2018 <a1759895304@gmail.com>
2022-03-04 22:32:57 +0800

    feat: stream

---
+++ cloud-stream-rabbitmq-provider-8801/src/main/java/com/atguigu/springcloud/service/impl/MessageProviderImpl.java
@@ -0,0 +1,25 @@
+package com.atguigu.springcloud.service.impl;
+
+import com.atguigu.springcloud.service.IMessageProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.MessageBuilder;
+
+import java.util.UUID;
+
+@EnableBinding(Source.class)
+public class MessageProviderImpl implements IMessageProvider
+{
+    @Autowired
+    private MessageChannel output;
+
+    @Override
+    public String send() {
+        String uuid = UUID.randomUUID().toString();
+        output.send(MessageBuilder.withPayload(uuid).build());
+        System.out.println("*****   " + uuid);
+        return null;
+    }
+}
